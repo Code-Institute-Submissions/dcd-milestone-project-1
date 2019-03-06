7bec374fbc14eff68f77c726b0de66f926a95a4b
Simon Law <simondw.law@outlook.com>
2019-03-06 20:42:30 +0000

    testing app deployment

--- app.py
+++ app.py
@@ -1,7 +1,6 @@
 import os
-from flask import Flask, render_template, redirect, request, url_for, request
-from flask_pymongo import PyMongo
-from bson.objectid import ObjectId
+from flask import Flask
+
 
 app = Flask(__name__)
 
